fe462e8a3b43565e1a0a673e16537492c56771c8
Amber <amber@dendriticspine.com>
2018-02-26 21:13:27 -0800

    adding beta email to admin

--- demo/admin.py
+++ demo/admin.py
@@ -18,8 +18,11 @@ class DemoSentenceAdmin(admin.ModelAdmin):
     list_display = ['english', 'kana']
 
 
+class BetaEmailAdmin(admin.ModelAdmin):
+    list_display = ['user_beta_email', 'user_beta_submitted']
+
 admin.site.register(DemoVocab, DemoVocabAdmin)
 admin.site.register(DemoPractice, DemoPracticeAdmin)
 admin.site.register(DemoSentence, DemoSentenceAdmin)
 admin.site.register(DemoExpression, DemoExpressionAdmin)
-admin.site.register(BetaEmail)
+admin.site.register(BetaEmail, BetaEmailAdmin)
