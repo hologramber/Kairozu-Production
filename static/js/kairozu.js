e55bdafba7e37c09bb9aae8bcde2529b3a277142
Amber <amber@dendriticspine.com>
2018-05-13 20:32:06 -0700

    moving js

---
+++ static/js/kairozu.js
@@ -0,0 +1,33 @@
+$(function() {
+    $("#byealert").click(function() { $(this).parent().remove(); });
+    $("#menu-toggle").click(function(e) {
+        e.preventDefault();
+        $("#wrapper").toggleClass("toggled");
+    });
+
+    $('#nav-toggle').click(function(e) {
+        $('#myTopnav').toggleClass("responsive");
+        $('.dropdown-content').hide();
+        $('.dropdown').removeClass('active');
+        e.stopPropagation();
+    });
+
+    $('.dropdown').click(function(e) {
+        $(this).children('.dropdown-content').slideToggle();
+        this.classList.toggle('active');
+        $('.dropdown-content').not($(this).children()).hide();
+        $('.dropdown').not($(this)).removeClass('active');
+        e.stopPropagation();
+    });
+
+    $('html').click(function(e) {
+        if (e.target.id == 'form-modal') {
+            e.target.style.display = "none";
+        } else {
+            $('.dropdown-content').hide();
+            $('.dropdown').removeClass('active');
+            $('#myTopnav').removeClass('responsive');
+        }
+        e.stopPropagation();
+    });
+});
